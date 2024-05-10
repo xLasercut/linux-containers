d2aed2a94ae0b7aa0de61f764247f96eb26cd86d
Tim Wang <tianxun.wang1@nhs.net>
2024-05-10 10:14:01 +0100

    added rpm fusion setup

--- silverblue/build.sh
+++ silverblue/build.sh
@@ -2,6 +2,11 @@
 
 set -ouex pipefail
 
+rpm-ostree install \
+  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+
 rpm-ostree override remove \
   firefox firefox-langpacks \
   gnome-terminal gnome-terminal-nautilus
