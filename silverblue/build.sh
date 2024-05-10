0a8298df1766c7341ef0d464c485a6ff3903e518
Tim Wang <tianxun.wang1@nhs.net>
2024-05-10 11:11:34 +0100

    moved repo to separate layer

--- silverblue/build.sh
+++ silverblue/build.sh
@@ -2,11 +2,6 @@
 
 set -ouex pipefail
 
-rpm-ostree install \
-  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
-  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
-
 rpm-ostree override remove \
   firefox firefox-langpacks \
   gnome-terminal gnome-terminal-nautilus \
---
+++ silverblue/repo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+rpm-ostree install \
+  "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
+  "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
