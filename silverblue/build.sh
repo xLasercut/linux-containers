6def0f4ef0c0c5cf59b4859f496dbeb8c1f780b7
Tim Wang <tianxun.wang1@nhs.net>
2024-05-14 08:53:26 +0100

    reverted pulseaudio change

--- silverblue/build.sh
+++ silverblue/build.sh
@@ -10,6 +10,7 @@ rpm-ostree override remove \
 rpm-ostree install \
   ffmpeg ffmpeg-libs gstreamer1-libav gstreamer1-plugin-openh264 gstreamer1-plugins-bad-freeworld gstreamer1-plugins-ugly mesa-va-drivers-freeworld \
   docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin \
+  gcc patch zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel xz-devel libuuid-devel gdbm-libs libnsl2 \
   screen \
   vim \
   make \
