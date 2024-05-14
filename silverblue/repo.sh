bd02432a1ebe98d70dff644259018f6b1e65eb94
Tim Wang <tianxun.wang1@nhs.net>
2024-05-14 09:14:00 +0100

    fixed rpmfusion repo

--- silverblue/repo.sh
+++ silverblue/repo.sh
@@ -5,8 +5,3 @@ set -ouex pipefail
 rpm-ostree install \
   "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
   "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
-
-rpm-ostree update --uninstall "$(rpm -q rpmfusion-free-release)" \
-  --uninstall "$(rpm -q rpmfusion-nonfree-release)" \
-  --install rpmfusion-free-release \
-  --install rpmfusion-nonfree-release
