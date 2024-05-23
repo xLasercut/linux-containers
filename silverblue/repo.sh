cf668e868b04ebd99f58edee6b6a7dfad9e4c1fc
Tim Wang <tianxun.wang1@nhs.net>
2024-05-23 10:31:44 +0100

    updated tmp rpm directory

--- silverblue/repo.sh
+++ silverblue/repo.sh
@@ -5,6 +5,8 @@ set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 RPMFUSION_MIRROR_RPMS="https://mirrors.rpmfusion.org"
 
+mkdir -p /tmp/rpms
+
 curl -Lo "/tmp/rpms/rpmfusion-free-release-${RELEASE}.noarch.rpm" "${RPMFUSION_MIRROR_RPMS}/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm"
 curl -Lo "/tmp/rpms/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm" "${RPMFUSION_MIRROR_RPMS}/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm"
 
