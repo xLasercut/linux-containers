c6aa399ba079df2a1d30835b7b99c312b6b73eb4
Tim Wang <tianxun.wang2004@gmail.com>
2024-06-11 15:24:59 +0100

    removed rpm fusion (#15)

--- silverblue/repo.sh
+++ silverblue/repo.sh
@@ -2,13 +2,13 @@
 
 set -ouex pipefail
 
-RELEASE="$(rpm -E %fedora)"
-RPMFUSION_MIRROR_RPMS="https://mirrors.rpmfusion.org"
-
-mkdir -p /tmp/rpms
-
-curl -Lo "/tmp/rpms/rpmfusion-free-release-${RELEASE}.noarch.rpm" "${RPMFUSION_MIRROR_RPMS}/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm"
-curl -Lo "/tmp/rpms/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm" "${RPMFUSION_MIRROR_RPMS}/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm"
-
-rpm-ostree install \
-    /tmp/rpms/*.rpm
+#RELEASE="$(rpm -E %fedora)"
+#RPMFUSION_MIRROR_RPMS="https://mirrors.rpmfusion.org"
+#
+#mkdir -p /tmp/rpms
+#
+#curl -Lo "/tmp/rpms/rpmfusion-free-release-${RELEASE}.noarch.rpm" "${RPMFUSION_MIRROR_RPMS}/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm"
+#curl -Lo "/tmp/rpms/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm" "${RPMFUSION_MIRROR_RPMS}/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm"
+#
+#rpm-ostree install \
+#    /tmp/rpms/*.rpm
