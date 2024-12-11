28cd5592dc134e8b42b8d58a5496b228f12a4db2
Tim Wang <tianxun.wang2004@gmail.com>
2024-12-11 09:38:00 +0000

    stopped silverblue container, added steam deck container
    * added vivaldi
    
    * updated distrobox containers
    
    * fixed distrobox build
    
    * fixed repo files for distrobox
    
    * updated java
    
    * test
    
    * test again
    
    * added gcc
    
    * removed pyenv dependencies
    
    * added steam deck image
    
    * renamed docker container
    
    * fixed typo

--- silverblue/build.sh
+++ silverblue/build.sh
@@ -7,7 +7,6 @@ rpm-ostree override remove \
 
 rpm-ostree install \
   docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin \
-  gcc patch zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel xz-devel libuuid-devel gdbm-libs libnsl2 \
   screen \
   vim \
   make \
@@ -28,7 +27,8 @@ rpm-ostree install \
   fastfetch \
   awscli2 \
   packer \
-  java-11-openjdk.x86_64
+  java-11-openjdk.x86_64 \
+  vivaldi-stable
 
 # ecr credential helper
 chmod +x /bin/docker-credential-ecr-login
